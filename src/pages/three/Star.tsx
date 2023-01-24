898d629bc6c1854804ca0fed841c5abd89f780b9
ATLANI_M <atlani-mylan@hotmail.fr>
2023-01-24 18:05:03 +0100

    implementation of threeJS, make 3D background and create star component with glowing animation

---
+++ src/pages/three/Star.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { extend } from '@react-three/fiber'
+import { UnrealBloomPass } from 'three-stdlib'
+extend({ UnrealBloomPass })
+
+function Star({ children, color, ...props }) {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef()
+    return (
+        <mesh
+            {...props}
+            scale={1}
+            ref={mesh}>
+            {children}
+            {/* Now, in order to get selective bloom we simply crank colors out of
+          their natural spectrum. Where colors are normally defined between 0 - 1 we push them
+          way out of range, into a higher defintion (HDR). What previously was [1, 1, 1] now could
+          for instance be [10, 10, 10]. This requires that toneMapping is off, or it clamps to 1 */}
+            <meshBasicMaterial color={color} toneMapped={false} />
+        </mesh>
+    )
+}
+export default function StarComponent(props) {
+    const { position } = props
+    const [shining, setShining] = useState(Math.floor(Math.random() * (5 - 0 + 1) + 0))
+    const [bloom, setBloom] = useState(0)
+    const [shiningPlus, setShiningPlus] = useState(false)
+
+    useEffect(() => {
+        setInterval(() => {
+            setBloom(bloom => bloom + 1)
+        }, 100)
+    }, [])
+
+    useEffect(() => {
+        if (shiningPlus === true) {
+            setShining(shining => shining - 0.01)
+        }
+        else {
+            setShining(shining => shining + 0.01)
+        }
+    }, [bloom])
+
+    useEffect(() => {
+        if (shining >= 1) {
+            setShiningPlus(true)
+        }
+        if (shining <= 0) {
+            setShiningPlus(false)
+        }
+    }, [shining])
+    useEffect(() => {
+        //console.log(shining)
+    }, [shining])
+    return (
+        <Star color={[shining, shining, shining]} position={position}>
+            <sphereGeometry args={[0.05, 10]} />
+        </Star>
+    )
+}
