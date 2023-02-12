5bc49c1be7db2e0cfd22799765fa211675176290
Céline <celine.emptoz-lacote@laplateforme.io>
2023-02-12 21:02:00 +0100

    add component title screen

--- src/components/Title_screen.tsx
+++ src/components/Title_screen.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 // Component allowing to display an adaptable title and subtitle with an adaptable top and bottom margin, titile's color,
 // size and font.
 export default function Title_screen({firstTitle, secondTitle, marginTop, marginBottom, colorFirstTitle, colorSecondTitle, fontFirstTitle, fontSecondTitle}) {
-   
     return (
       <div style={{marginTop: marginTop , marginBottom: marginBottom}}>
         <h2 style={{color: colorSecondTitle ,font: fontSecondTitle}}>{secondTitle}</h2>
