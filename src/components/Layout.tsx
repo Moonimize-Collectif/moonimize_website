9b8d4bb886d1ae3c8ecb577869a520e91a12658c
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2023-02-13 14:47:37 +0100

    navbar + layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+// components/layout.js
+import { Navbar } from './Navbar';
+// import Footer from './footer'
+import styles from '@/styles/Layout.module.css';
+
+export default function Layout({ children }:any) {
+  return (
+    <div className={styles.layout}>
+      <Navbar />
+      <main style={{width: "100%", height:"100%"}}>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import ThreeLandingBackground from '@/pages/three/background_landing_page'
+import ThreeLandingBackground from '@/pages/three/background_landing_page';
+// import { Navbar } from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,9 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main style={{width: "100%", height:"100%"}}>
-        <ThreeLandingBackground/>
-      </main>
+      <ThreeLandingBackground/>
     </div>
   )
 }
