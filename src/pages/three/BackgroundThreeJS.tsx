d260b9c72fb88870131f39d0a7bab5b6bf612bea
ATLANI_M <atlani-mylan@hotmail.fr>
2023-02-15 15:14:44 +0100

    Add level system for background and white line crossing website

--- src/pages/three/BackgroundThreeJS.tsx
+++ src/pages/three/BackgroundThreeJS.tsx
@@ -3,9 +3,9 @@ import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame, useLoader, extend } from '@react-three/fiber'
 import { Effects } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
-import {StarComponent} from './Star'
-import SunMoon from "./SunMoon"
-import {RotatingLight} from "./RotatingLight"
+import { StarComponent } from './Star'
+import styles from '@/styles/BackgroundThreeJS.module.css'
+import { RotatingLight } from "./RotatingLight"
 extend({ UnrealBloomPass })
 
 
@@ -36,14 +36,14 @@ function randomCoordinates() {
   let z = Math.floor(Math.random() * (200 - 10 + 1) + 100)
   //let x = Math.floor(Math.random() * 60) + -30
   //let x = Math.random() >= 0.5 ? Math.random() * (-(8.1 + (z * 0.158)) - (0 + (z * 0.158)) + 1) + (0 + (z * 0.158)) : Math.random() * ((8.1 + (z * 0.158)) - (0 + (z * 0.158)) + 1) + (0 + (z * 0.158));
- /* let y = x > 1 + (z * 0.158 / 2) ?
-    Math.random() >= 0.5 ? Math.random() * (-(8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2)) : Math.random() * ((8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2))
-    : Math.random() >= 0.5 ? Math.random() * (-(8 / 2 + (z * 0.158 / 2)) - (1 + (z * 0.158 / 2)) + 1) + (1 + (z * 0.158 / 2)) : Math.random() * ((8 / 2 + (z * 0.158 / 2)) - (1 + (z * 0.158 / 2)) + 1) + (1 + (z * 0.158 / 2));*/
+  /* let y = x > 1 + (z * 0.158 / 2) ?
+     Math.random() >= 0.5 ? Math.random() * (-(8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2)) : Math.random() * ((8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2))
+     : Math.random() >= 0.5 ? Math.random() * (-(8 / 2 + (z * 0.158 / 2)) - (1 + (z * 0.158 / 2)) + 1) + (1 + (z * 0.158 / 2)) : Math.random() * ((8 / 2 + (z * 0.158 / 2)) - (1 + (z * 0.158 / 2)) + 1) + (1 + (z * 0.158 / 2));*/
   let minX = (-(8.1 + (z * 0.158)) - (0 + (z * 0.158)) + 1) + (0 + (z * 0.158))
   let maxX = ((8.1 + (z * 0.158)) - (0 + (z * 0.158)) + 1) + (0 + (z * 0.158))
   let minY = (-(8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2))
   let maxY = ((8 / 2 + (z * 0.158 / 2)) - (0 + (z * 0.158 / 2)) + 1) + (0 + (z * 0.158 / 2))
-  let x = Math.floor(Math.random() * (maxX + 30) + (minX)-2)
+  let x = Math.floor(Math.random() * (maxX + 30) + (minX) - 2)
   let y = Math.floor(Math.random() * (maxY + 20) + (minY) - 2)
   return [x, y, z]
 }
@@ -82,15 +82,23 @@ export const ThreeBackground = () => {
   }, [])
 
   return (
-    <Canvas camera={{ zoom: 1, fov: 10, near: 0.01, far: 1000, position: [0, 0, -45] }}>
-      <RotatingLight />
-      <Effects disableGamma>
-        <unrealBloomPass threshold={1} strength={3} radius={1} />
-      </Effects>
-      {stars.map((star, index) => {
-        return <StarComponent key={index} position={star.coordinates} />
-      })
-      }
-    </Canvas>
+    <div className={styles.ThreeContainer}>
+      <div className={styles.StarDiv}>
+        <Canvas camera={{ zoom: 1, fov: 10, near: 0.01, far: 1000, position: [0, 0, -45] }}>
+          <Effects disableGamma>
+            <unrealBloomPass threshold={1} strength={3} radius={1} />
+          </Effects>
+          {stars.map((star, index) => {
+            return <StarComponent key={index} position={star.coordinates} />
+          })
+          }
+        </Canvas>
+      </div>
+      <div className={styles.MoonDiv}>
+        <Canvas camera={{ zoom: 1, fov: 10, near: 0.01, far: 1000, position: [0, 0, -45] }}>
+          <RotatingLight />
+        </Canvas>
+      </div>
+    </div>
   )
 }
