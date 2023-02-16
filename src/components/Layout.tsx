c5fb9b1e46a78a8c691ae5dc4996ff9243986b3f
Ay-ukii <remy.viscaino@gmail.com>
2023-02-16 10:00:25 +0100

    update footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,15 +3,16 @@ import { Navbar } from './Navbar';
 import styles from '@/styles/Layout.module.css';
 import {BackgroundLine} from '@/components/BackgroundLine'
 import {Footer} from "@/components/Footer";
+import {ThreeBackground} from "@/pages/three/BackgroundThreeJS";
 
 export default function Layout({ children }:any) {
   return (
     
     <div className={styles.layout}>
-      <BackgroundLine></BackgroundLine>
+        <ThreeBackground/>
+        <BackgroundLine></BackgroundLine>
       <Navbar />
-      <main style={{width: "100%", height:"100%"}}>{children}</main>
-        <Footer />
+      <main style={{width: "100%", height:"100%", position: "relative"}} >{children}</main>
     </div>
   )
 }
