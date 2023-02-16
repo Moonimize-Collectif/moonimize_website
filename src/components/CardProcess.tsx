2e7281c0ba929d041f1f7825d5b3d30d8bd87eca
Céline <celine.emptoz-lacote@laplateforme.io>
2023-02-16 07:29:42 +0100

    add styles

--- src/components/CardProcess.tsx
+++ src/components/CardProcess.tsx
@@ -1,10 +1,15 @@
 import style from '@/styles/globals.css'
 import styles from '@/styles/CardProcess.module.css'
 
-export const CardProcess = ({number, title, description} :any) => {
+export const CardProcess = ({number, title, description, positionNumber} :any) => {
     return (
-        <div className={styles.divCardProcess} style={{width: "66%", margin: "auto"}}>
-            <p className={styles.pCard}>{number}</p>
+        <div className={styles.divCardProcess} style={{width: "66%", margin: "2% auto"}}>
+            {positionNumber == 'right' ?
+                <p className={styles.pCardRight}>{number}</p>
+                :
+                <p className={styles.pCardLeft}>{number}</p>
+            }
+            {/* <p className={styles.pCard}>{number}</p> */}
             <h2 className={styles.titleCard}>{title}</h2>
             <p className={styles.describCard}>{description}</p>
         </div>
