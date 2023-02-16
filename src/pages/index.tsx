00cae2b0776484b4858e5657295f0abf9a881bbf
ATLANI_M <atlani-mylan@hotmail.fr>
2023-02-16 09:52:01 +0100

    add background color to visualize scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,20 +11,21 @@ export default function Home() {
   return (
     <div style={{ position: "relative", top: 0, width: "100%", minHeight: "100vh" }}>
       <Moon />
-        <div style={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-            <TitleScreen
-            firstTitle={"MOONIMIZE"}
-            secondTitle={"Collectif"}
-            marginTop={"20%"}
-            marginBottom={"0%"}
-            colorFirstTitle={"white"}
-            colorSecondTitle={"#1ACAD5"}
-            sizeSecondTitle={"140px"}
-            sizeFirstTitle={"120px"}
-            textAlign={"center"}
-            paddingLeft={"0"}
-            />
-        </div>
+      <div style={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <TitleScreen
+          firstTitle={"MOONIMIZE"}
+          secondTitle={"Collectif"}
+          marginTop={"20%"}
+          marginBottom={"0%"}
+          colorFirstTitle={"white"}
+          colorSecondTitle={"#1ACAD5"}
+          sizeSecondTitle={"140px"}
+          sizeFirstTitle={"120px"}
+          textAlign={"center"}
+          paddingLeft={"0"}
+        />
+      </div>
+      <div style={{ backgroundColor: "#2d2d2d" }}>
         <h2>Bonjour</h2>
         <h2>Bonjour</h2>
         <h2>Bonjour</h2>
