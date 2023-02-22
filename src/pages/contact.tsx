1baf4245bd0341aabf66f466ffa09bc7c4a97968
Céline <celine.emptoz-lacote@laplateforme.io>
2023-02-22 08:28:48 +0100

    add title screen

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,21 +1,39 @@
 import styles from "@/styles/Members.module.css";
-import PageTitle from "@/components/PageTitle";
+import Titlescreen from "@/components/TitleScreen";
 import { fetchWrapper } from "@/utils/fetchWrapper";
 import { ContactForm } from "@/components/ContactForm";
+import useScreenSize from "../hooks/useScreenSize";
+import { useEffect, useState } from "react";
 
 const Contact = ({}) => {  
+    const screenWidth = useScreenSize().width;
+    const [firstTitleSize, setFirstTitleSize] = useState("0px")
+    const [secondTitileSize, setSecondTitleSize] = useState("0px")
+    console.log(screenWidth)
+    useEffect(() => {
+        if (screenWidth > 670){
+            setFirstTitleSize("140px")
+            setSecondTitleSize("120px")
+        }else {
+            setFirstTitleSize("110px")
+            setSecondTitleSize("90px")
+        }    
+    });
   
   return (
     <div className="page">
-      <div className={styles.pageTitleContainer}>
-        <PageTitle
-            mainTitle={"Contact"}
-            subTitle={"Allo Houston !"}
-            sizeMainTitle="140px"
-            sizeSubTitle="120px"
-            colorSubTitle="#1ACAD5"
-            textAlign="center"
-            marginTop="-110px"
+      <div className={styles.pageTitleContainer} style={{height: "20vh"}}>
+        <Titlescreen 
+        firstTitle={"Contact"} 
+        secondTitle={"Allo Houston !"} 
+        marginTop={""} 
+        marginBottom={""}
+        colorFirstTitle={"white"}
+        colorSecondTitle={"#1ACAD5"}
+        sizeFirstTitle={firstTitleSize}
+        sizeSecondTitle={secondTitileSize}
+        textAlign={"center"}
+        paddingLeft={""}
         />
       </div>
       <div className={styles.membersContainer}>
